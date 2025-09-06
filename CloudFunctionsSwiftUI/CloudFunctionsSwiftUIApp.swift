36e2727e035c4c9e17aac46759e7353dd419e821
Kevinho Morales <kfmorales94@gmail.com>
2025-09-06 11:19:44 -0500

    Firebase Init project

---
+++ CloudFunctionsSwiftUI/CloudFunctionsSwiftUIApp.swift
@@ -0,0 +1,29 @@
+//
+//  CloudFunctionsSwiftUIApp.swift
+//  CloudFunctionsSwiftUI
+//
+//  Created by Kevinho Morales on 6/9/25.
+//
+
+import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct CloudFunctionsSwiftUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
